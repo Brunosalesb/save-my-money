dc8442014cc5dba3e7ef2c957b6a244db8651380
Bruno Sales <bruno.sbernardes01@gmail.com>
2020-08-13 23:37:33 -0300

    add migration

--- SaveMyMoney.Infra/Contexts/SaveMyMoneyDataContext.cs
+++ SaveMyMoney.Infra/Contexts/SaveMyMoneyDataContext.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SaveMyMoney.Domain.Entities;
 using SaveMyMoney.Infra.Mappings;
+using System.Configuration;
 
 namespace SaveMyMoney.Infra.Contexts
 {
     public class SaveMyMoneyDataContext : DbContext
     {
-        public SaveMyMoneyDataContext(DbContextOptions<SaveMyMoneyDataContext> options) : base(options) {}
+        //here so i can use migration
+        public SaveMyMoneyDataContext() { }
+        public SaveMyMoneyDataContext(DbContextOptions<SaveMyMoneyDataContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\sqlexpress;Database=SaveMyMoney;Integrated Security=True");
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new UserMap());
